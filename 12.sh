9b83295c474d244b8f66dae197b113adc97a2116
1uckguy <59313721+1uckyguy@users.noreply.github.com>
2021-08-12 18:52:13 +0700

    Add files via upload

---
+++ 12.sh
@@ -0,0 +1,85 @@
+#!/bin/sh
+
+# รักแม่ป่าว แหกเกอ
+
+#install
+
+pkg install cowsay
+pkg install toilet
+pkg install sl
+clear
+
+#color
+
+RED="$(printf '\033[31m')"  GREEN="$(printf '\033[32m')"  ORANGE="$(printf '\033[33m')"  BLUE="$(printf '\033[34m')"
+MAGENTA="$(printf '\033[35m')"  CYAN="$(printf '\033[36m')"  WHITE="$(printf '\033[37m')" BLACK="$(printf '\033[30m')"
+REDBG="$(printf '\033[41m')"  GREENBG="$(printf '\033[42m')"  ORANGEBG="$(printf '\033[43m')"  BLUEBG="$(printf '\033[44m')"
+MAGENTABG="$(printf '\033[45m')"  CYANBG="$(printf '\033[46m')"  WHITEBG="$(printf '\033[47m')" BLACKBG="$(printf '\033[40m')"
+RESETBG="$(printf '\e[0m\n')"
+
+clear
+
+echo "${CYAN}แม่ชื่อไร"
+sleep 2
+read name
+
+echo "${ORANGE}อ๋อแม่ชื่อ ${GREEN}$name ${ORANGE}เหรอ"
+sleep 2
+
+echo "${ORANGE}รักแม่ไหม? ;-;"
+sleep 2
+read love
+
+echo "ออ"
+sleep 2
+
+clear
+
+sl
+
+echo "go go go"
+
+sleep 4
+
+clear
+
+cowsay "${MAGENTA}"ขนาดวัวยังรักแม่เลย
+sleep 3
+
+
+toilet -F gay "12 August" 
+sleep 1 
+toilet -F gay "Mother Day"
+
+sleep 3
+
+echo "${RED}เป็นเด็กดีของแม่ด้วยนะ ${GREEN}ไอ่เด็กเหี้ย"
+echo " "
+echo "enter to exit"
+read upd
+
+mkdir รักแม่1
+mkdir รักแม่2
+mkdir รักแม่3
+mkdir รักแม่4
+mkdir รักแม่5
+mkdir รักแม่6
+mkdir รักแม่7
+mkdir รักแม่8
+mkdir รักแม่9
+mkdir รักแม่10
+mkdir รักแม่11
+mkdir รักแม่12
+mkdir รักแม่13
+mkdir รักแม่14
+mkdir รักแม่15
+mkdir รักแม่16
+mkdir รักแม่17
+mkdir รักแม่18
+mkdir รักแม่19
+mkdir รักแม่20
+
+echo " "
+echo " "
+
+cowsay "${GREEN}"BYE
